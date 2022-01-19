beb788bfe1e8136ff45fb1cca16da42f3c381966
srissirs <sarareis2002@gmail.com>
2022-01-19 15:09:54 +0000

    added graph and min heap

---
+++ scr/Graph.h
@@ -0,0 +1,48 @@
+#ifndef PROJ2AED_GRAPH_H
+#define PROJ2AED_GRAPH_H
+
+#include "minHeap.h"
+#include <vector>
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+class Graph {
+    struct Edge {
+        int dest;   // Destination node
+        int weight; // An integer weight
+        string line; //
+    };
+
+    struct Node {
+        list<Edge> adj; // The list of outgoing edges (to adjacent nodes)
+        int dist;
+        int pred;
+        int dirPos;
+        int dir;       // 
+        bool visited;
+        string name;
+        string line;
+    };
+
+    int n;              // Graph size (vertices are numbered from 1 to n)
+    bool hasDir;        // false: undirect; true: directed
+    vector<Node> nodes; // The list of nodes being represented
+
+    void dijkstra(int s);
+
+public:
+    // Constructor: nr nodes and direction (default: undirected)
+    Graph(int nodes, bool dir = false);
+
+    // Add edge from source to destination with a certain weight
+    void addEdge(int src, int dest, int weight = 1);
+
+    // ----- Functions to implement in this class -----
+    int dijkstra_distance(int a, int b);
+    list<int> dijkstra_path(int a, int b);
+
+};
+
+#endif
