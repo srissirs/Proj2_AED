70f6dc629ccad07b6195ddcf35705e1362fdb449
Hugo <hfralmeida@gmail.com>
2022-01-19 15:57:16 +0000

    Added dataset

---
+++ codigo/scr/readFiles.cpp
@@ -0,0 +1,6 @@
+#include "readFiles.h"
+#include <fstream>
+
+void readDataStops(Graph &graph){
+    ifstream airplanesFile("dataset/stops.txt");
+}
---
+++ codigo/scr/readFiles.h
@@ -0,0 +1,8 @@
+#ifndef PROJ2AED_READFILES_H
+#define PROJ2AED_READFILES_H
+#include "Graph.h"
+
+void readDataStops(Graph &graph);
+
+
+#endif //PROJ2AED_READFILES_H
