9dc01c9001017b1b986cef230ac7b4e80a1ba1cd
Teresa Ferreira <teresa.t.ferreira@hotmail.com>
2022-01-25 16:40:32 +0000

    menu

---
+++ codigo/scr/menu.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "menu.h"
+
+using namespace std;
+void displayMainMenu(){
+    cout << "-------------MENU-------------"<<endl;
+    cout << "[0] Choose stops by their code"<<endl;
+    cout << "[1] Choose stops by coordinates" << endl;
+}
+
+void displayStopsCodeMenu(){
+    cout << "-------------MENU-------------"<<endl;
+    cout << "[0] Choose stops by their code"<<endl;
+    cout << "[1] Choose stops by coordinates" << endl;
+}
+
+
+void displaysCoordinatesMenu(){
+    cout << "-------------MENU-------------"<<endl;
+    cout << "[0] Choose stops by their code"<<endl;
+    cout << "[1] Choose stops by coordinates" << endl;
+}
+
+
+void displaysMethods(){
+    cout << "-------------MENU-------------"<<endl;
+    cout << "[0] Best price"<<endl;
+    cout << "[1] Less changes between lines" << endl;
+    cout << "[2] Less bus stops" << endl;
+    cout << "[3] Smallest distance" << endl;
+}
---
+++ codigo/scr/menu.h
@@ -0,0 +1,10 @@
+#ifndef PROJ2AED_MENU_H
+#define PROJ2AED_MENU_H
+
+
+void displayMainMenu();
+void displayStopsCodeMenu();
+void displaysCoordinatesMenu();
+void displaysMethods();
+
+#endif //PROJ2AED_MENU_H
